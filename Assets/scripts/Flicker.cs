9f2cb3be8a1a18a83d0a8586e5c96a9550367aa8
jggiles <jggiles@gmail.com>
2018-01-10 10:17:16 -0500

    -Added LeanTween -Built a sprite fading system -Built a flickering system -Assembled title images

---
+++ Assets/scripts/Flicker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flicker : MonoBehaviour
+{
+    public RectTransform itemToFlicker;
+    public float         frequency;
+    public float         minimum;
+    public float         maximum;
+    public bool          debug;
+
+    private IEnumerator  _coroutine;
+
+    void Start()
+    {
+        _coroutine = FlickerSprite(frequency, minimum, maximum);
+        StartCoroutine(_coroutine);
+    }
+
+    private IEnumerator FlickerSprite(float freq, float min, float max)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(freq);
+            itemToFlicker.GetComponent<CanvasRenderer>().SetAlpha(UnityEngine.Random.Range(min, max));
+            if (debug)
+            {
+                print("flickering " + Time.time + ", Alpha: " + itemToFlicker.GetComponent<CanvasRenderer>().GetAlpha());
+            }
+        }
+    }
+}
---
+++ Assets/scripts/SpriteFader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteFader : MonoBehaviour
+{
+    public List<RectTransform> sprites;
+    public float               fadeFrequency;
+    public float               fadeLength;
+    public bool                debug;
+
+    private IEnumerator        _coroutine;
+    private int                _listPosition = 0;
+
+    void Start()
+    {
+        _coroutine = Fader(fadeFrequency);
+        StartCoroutine(_coroutine);
+    }
+
+    //A timer that fades through an array of sprites in sequence
+    private IEnumerator Fader(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(fadeFrequency);
+
+            //fade this sprite out, fade in next two sprites
+            LeanTween.alpha(sprites[_listPosition], 0, fadeLength);
+            LeanTween.alpha(sprites[IncrimentListPosition(_listPosition, sprites.Count)], 1, fadeLength);
+            LeanTween.alpha(sprites[IncrimentListPosition(_listPosition + 1, sprites.Count)], 1, fadeLength);
+            _listPosition = IncrimentListPosition(_listPosition, sprites.Count);
+
+            if (debug)
+            {
+                print("fading " + Time.time + ", List Position: " + _listPosition);
+            }
+        }
+    }
+
+    private int IncrimentListPosition(int position, int max)
+    {
+        if (position >= max - 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return position + 1;
+        }
+    }
+}
